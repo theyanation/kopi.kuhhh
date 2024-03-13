4abb6062712b7f9b75f4f2986c26d874daab6a67
theyanation <hudukaja@gmail.com>
2024-03-13 20:40:44 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// toggle class active
+
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+// ketika hamburger menu di click
+
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+
+// klik di luar sidebar dimanapun halaman 
+
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    };
+});
+
